ed4a8be502a157c42b2119944da1c0dc5865c260
Alex Spec <alexyankovskio@gmail.com>
2025-05-26 20:00:41 +0200

    add: requirements.txt, docker-compose.yml, dockerfile.tests, updated: README.md

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,38 @@
+import time
+import os
 import pytest
+import requests
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
-    # Browser settings
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL")
+    options = Options()
 
-    #  Launch WebDriver
-    driver = webdriver.Chrome(
-        options=options,
-        service=ChromeService(ChromeDriverManager().install())
-    )
+    if selenium_url:
+        for i in range(10):
+            try:
+                response = requests.get(selenium_url + "/status")
+                if response.status_code == 200:
+                    break
+            except Exception:
+                time.sleep(1)
+        else:
+            raise RuntimeError("Selenium не отвечает после 10 попыток")
+
+        driver = webdriver.Remote(
+            command_executor=selenium_url,
+            options=options
+        )
+    else:
+        from selenium.webdriver.chrome.service import Service as ChromeService
+        from webdriver_manager.chrome import ChromeDriverManager
+        driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),
+            options=options
+        )
 
     yield driver
-
-    # Close the browser after the test
-    print("End test")
     driver.quit()
-
 
